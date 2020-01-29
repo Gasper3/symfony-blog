071b3fb56a84ff507b5f626e33a4bb8ab79e3cc2
Gasper3 <trzecik65@gmail.com>
2020-01-29 19:41:59 +0100

    TimestampableEntity for article and comment

--- src/DataFixtures/ArticleFixture.php
+++ src/DataFixtures/ArticleFixture.php
@@ -10,20 +10,8 @@ class ArticleFixture extends BaseFixture
 {
     protected function loadData(ObjectManager $manager)
     {
-//        for ($i=1;$i<=5;$i++)
-//        {
-//            $article = new Article();
-//            $article->setTitle($this->faker->title)
-//                ->setSlug($this->faker->slug)
-//                ->setPublishedAt($this->faker->dateTimeBetween('-10 days', '-1 days'))
-//                ->setContent($this->faker->text($maxNbChars=1000))
-//                ->setImageFilename($this->faker->imageUrl($width=200, $height=140));
-//            $manager->persist($article);
-//        }
-
         $this->createMany(Article::class, 10, function(Article $article, $count) {
-            $article->setTitle($this->faker->title)
-                ->setSlug($this->faker->slug)
+            $article->setTitle($this->faker->sentence())
                 ->setPublishedAt($this->faker->dateTimeBetween('-10 days', '-1 days'))
                 ->setContent($this->faker->text($maxNbChars=1000))
                 ->setImageFilename($this->faker->imageUrl($width=200, $height=140));
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,12 +5,16 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
  */
 class Article
 {
+    use TimestampableEntity;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -25,6 +29,7 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=100, unique=true)
+     * @Gedmo\Slug(fields={"title"})
      */
     private $slug;
 
