af3cb2da00d80ec0c11ef99ee19242607a9bb6da
Gasper3 <trzecik65@gmail.com>
2020-02-19 20:17:20 +0100

    Added fontawesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
 import '../css/login.css';
-import '../css/main.css';
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
