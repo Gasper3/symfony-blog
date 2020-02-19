8e15a3f6db20953df84937d7b35a70f9a7b71db8
Gasper3 <trzecik65@gmail.com>
2020-02-19 20:11:14 +0100

    Added webpack encore

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
